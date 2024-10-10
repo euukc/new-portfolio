8315d766a45fbba92685e8c1484bbde4055b81d0
euukc <keniacarolineferreira@gmail.com>
2024-10-09 21:23:12 -0300

    Js adjustment

---
+++ inicio.js
@@ -0,0 +1,7 @@
+const whatsappButton = document.getElementById('whatsappButton');
+
+
+
+whatsappButton.addEventListener('click', function () {
+    window.location.href = 'https://wa.me/11994929467';
+});
